61bb8fc58d43069654389dcbe2b1dd319aada840
ho thi tuyet <hotuyet214@gmail.com>
2021-05-25 14:51:38 +0700

    nop bai

---
+++ ch02/mpg.js
@@ -0,0 +1,8 @@
+
+  var miles = prompt("Enter miles driven");
+  miles = parseFloat(miles);
+  var gallons = prompt("Enter gallons of gas used");
+  gallons = parseFloat(gallons);
+  var mpg = miles / gallons;
+  mpg = parseInt(mpg);
+ 
